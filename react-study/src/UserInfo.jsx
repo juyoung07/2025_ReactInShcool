5176465d9fdbc968718b8aec673f28bab404f0dc
juyoung07 <yunju5090@gmail.com>
2025-03-20 19:24:37 +0900

    props : 객체 비구조화 할당

--- react-study/src/UserInfo.jsx
+++ react-study/src/UserInfo.jsx
@@ -1,8 +1,18 @@
-function UserInfo(props) {
+// function UserInfo(props) {
+// 	return (
+// 		<h1>
+// 			name: {props.name}, age: {props.age}
+// 		</h1>
+// 	);
+// }
+
+// 파라미터에서 "객체 비구조화 할당"을 수행
+function UserInfo({ name, age }) {
 	return (
 		<h1>
-			name: {props.name}, age: {props.age}
+			name: {name}, age: {age}
 		</h1>
 	);
 }
+
 export default UserInfo;
