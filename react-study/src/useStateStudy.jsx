944c939bac21c0238d43b238c18febc4a864f3e9
juyoung07 <yunju5090@gmail.com>
2025-03-25 18:42:17 +0900

    [hook] : 객체를 이용.. useState

--- react-study/src/useStateStudy.jsx
+++ react-study/src/useStateStudy.jsx
@@ -4,17 +4,28 @@ import { useState } from "react";
 // useState, useEffect, useRef 같은 훅이 존재 (다른 훅도 있고 직접 만들 수도 있음)
 
 function UseStateStudy() {
-	// useState(초기값)
-	// 초기에는 [0, 세터함수] <= 이렇게 생긴 배열 반환
-	// 배열 비구조화 할당을 함
-	const [count, setCount] = useState(0);
-
+	const [data, setData] = useState({ count: 0 });
 	return (
 		<div>
-			<h1>{count}</h1>
-			{/* onClick={() => setCount(count + 1)}는 직접 증가시키기 때문에 함수로 쓰기 */}
-			<button onClick={() => setCount((c) => c + 1)}>증가</button>
-			<button onClick={() => setCount((c) => c - 1)}>감소</button>
+			<h1>{data.count}</h1>
+			<button
+				onClick={() =>
+					setData((prev) => {
+						return { ...prev, count: prev.count + 1 };
+					})
+				}
+			>
+				+
+			</button>
+			<button
+				onClick={() =>
+					setData((prev) => {
+						return { ...prev, count: prev.count - 1 };
+					})
+				}
+			>
+				-
+			</button>
 		</div>
 	);
 }
