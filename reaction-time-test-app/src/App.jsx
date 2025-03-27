d552f00a66acc1467008bdd08e7ee065aed7a084
juyoung07 <yunju5090@gmail.com>
2025-03-27 18:53:47 +0900

    반응속도 테스트 : 랜덤 및 css 적용

--- reaction-time-test-app/src/App.jsx
+++ reaction-time-test-app/src/App.jsx
@@ -1,11 +1,56 @@
 import { act, useState } from "react";
 
+const MIN_DELAY_MS = 2000;
+const MAX_ADDITIONAL_TIME = 1000;
+
+const DEACTIVE_COLOR = "grey";
+const ACTIVE_COLOR = "green";
+
+const colorByState = {
+	ready: DEACTIVE_COLOR,
+	start: DEACTIVE_COLOR,
+	test: ACTIVE_COLOR,
+	end: DEACTIVE_COLOR,
+};
+
+const defaultButtonStyle = {
+	width: 200,
+	height: 50,
+	fontSize: 26,
+	color: "white",
+	border: "0px",
+	borderRadius: 10,
+};
+
+const containerStyle = {
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	height: "100vh",
+	textAlign: "center",
+};
+
+const contentBoxStyle = {
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	width: "500px",
+	padding: "30px",
+	border: "2px solid black",
+	borderRadius: "10px",
+	backgroundColor: "white",
+};
+
 function App() {
 	const [state, setState] = useState("ready"); // 상태
 	const [record, setRecord] = useState({
 		start: null,
 		end: null,
 	});
+
+	const backgroundColor = colorByState[state];
 
 	let instruction; // 지금은 undefined
 	let actionButton;
@@ -14,6 +59,7 @@ function App() {
 		instruction = <h1>버튼을 누르면 시작합니다.</h1>;
 		actionButton = (
 			<button
+				style={{ backgroundColor }}
 				onClick={() => {
 					setState("start");
 					setTimeout(() => {
@@ -30,13 +76,15 @@ function App() {
 		);
 	} else if (state === "start") {
 		instruction = <h1>버튼이 녹색이 되면 누르세요.</h1>;
-		actionButton = <button>클릭</button>;
+		actionButton = <button style={{ backgroundColor }}>클릭</button>;
 	} else if (state === "test") {
 		instruction = <h1>클릭하세요!</h1>;
 		actionButton = (
 			<button
-				style={{ background: "green" }}
+				style={{ ...defaultButtonStyle, backgroundColor }}
 				onClick={() => {
+					const randomTime =
+						Math.random() * MAX_ADDITIONAL_TIME + MIN_DELAY_MS;
 					setState("end");
 
 					// end 기록
@@ -51,14 +99,21 @@ function App() {
 		const reactionTime = record.end - record.start;
 		instruction = <h1>반응 속도 : {reactionTime}ms</h1>;
 		actionButton = (
-			<button onClick={() => setState("ready")}>다시하기</button>
+			<button
+				style={{ backgroundColor }}
+				onClick={() => setState("ready")}
+			>
+				다시하기
+			</button>
 		);
 	}
 
 	return (
-		<div>
-			{instruction}
-			{actionButton}
+		<div style={{ ...containerStyle, backgroundColor }}>
+			<div style={contentBoxStyle}>
+				{instruction}
+				{actionButton}
+			</div>
 		</div>
 	);
 }
--- reaction-time-test-app/src/main.jsx
+++ reaction-time-test-app/src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./reset.css"; // ts에선 에러가 남 (import는 객체 등을 가져오는 것이기 때문.)
 
 createRoot(document.getElementById("root")).render(<App />);
