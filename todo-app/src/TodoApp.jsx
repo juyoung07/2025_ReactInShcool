0e7a3191d69e3b43923913ce0b64372981bb25ed
juyoung07 <yunju5090@gmail.com>
2025-04-01 19:53:56 +0900

    initial commit

---
+++ todo-app/src/TodoApp.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function TodoApp() {
+	return <div>TodoApp</div>;
+}
+
+export default TodoApp;
---
+++ todo-app/src/main.jsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client";
+import TodoApp from "./TodoApp.jsx";
+
+createRoot(document.getElementById("root")).render(<TodoApp />);
