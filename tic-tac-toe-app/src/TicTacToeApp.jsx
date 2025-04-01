1859fdbd661006ad97896861ee65f28e6cbd1cd3
juyoung07 <yunju5090@gmail.com>
2025-04-01 19:31:04 +0900

    게임 결과 및 스타일 적용

--- tic-tac-toe-app/src/TicTacToeApp.jsx
+++ tic-tac-toe-app/src/TicTacToeApp.jsx
@@ -20,6 +20,16 @@ const tileStyle = {
 	border: "2px solid gray",
 };
 
+// o, x 타일 스타일 추가
+const oStyle = {
+	color: "white",
+	background: "black",
+};
+const xStyle = {
+	color: "black",
+	background: "white",
+};
+
 function checkWinner(tiles) {
 	for (let i = 0; i < 9; i += 3) {
 		if (
@@ -49,21 +59,20 @@ function checkWinner(tiles) {
 	return null;
 }
 
-function Tile({ type, index, turn, setTurn, setGameBoard }) {
+function Tile({ type, index, handleTileClick }) {
+	// 스타일 적용..
+	// if (type !== null) {
+	// 	if (type === "o") finalStyle = { ...tileStyle, ...oStyle };
+	// 	if (type === "x") finalStyle = { ...tileStyle, ...xStyle };
+	// }
+
 	return (
 		<button
-			onClick={() => {
-				// 로직
-				if (type === null) {
-					setGameBoard((board) => {
-						const copy = [...board];
-						copy[index] = turn;
-						return copy;
-					});
-					setTurn((turn) => (turn === "o" ? "x" : "o"));
-				}
+			onClick={() => handleTileClick(type, index)}
+			style={{
+				...tileStyle,
+				...(type === null ? {} : type === "o" ? oStyle : xStyle),
 			}}
-			style={tileStyle}
 		>
 			{type !== null ? type : "-"}
 		</button>
@@ -73,24 +82,67 @@ function Tile({ type, index, turn, setTurn, setGameBoard }) {
 function TicTacToeApp() {
 	const [turn, setTurn] = useState("o");
 	const [gameBoard, setGameBoard] = useState(Array(9).fill(null));
+	const [winner, setWinner] = useState(null);
 
 	useEffect(() => {
-		console.log(checkWinner(gameBoard));
+		const currentWinner = checkWinner(gameBoard);
+		if (currentWinner === null) {
+			// 모든 타일이 null이 아닐 때
+			if (gameBoard.every((tile) => tile !== null)) {
+				setWinner("draw");
+			}
+		} else {
+			setWinner(currentWinner);
+		}
+
+		// console.log(checkWinner(gameBoard));
 	}, [gameBoard]);
+
+	const handleTileClick = (type, index) => {
+		if (type === null && winner === null) {
+			setGameBoard((board) => {
+				const copy = [...board];
+				copy[index] = turn;
+				return copy;
+			});
+			setTurn((turn) => (turn === "o" ? "x" : "o"));
+		}
+	};
 
 	return (
 		<div>
-			<h1>Current Turn : {turn}</h1>
-			next Turn : {turn === "o" ? "x" : "o"}
+			{winner === null ? (
+				<div>
+					<h1>Current Turn : {turn}</h1>
+					next Turn : {turn === "o" ? "x" : "o"}
+				</div>
+			) : (
+				<div>
+					{winner === "draw" ? (
+						<h1>비겼습니다.</h1>
+					) : (
+						<h1>{winner}가 이겼습니다.</h1>
+					)}
+					<button
+						onClick={() => {
+							setTurn("o");
+							setGameBoard(Array(9).fill(null));
+							setWinner(null);
+						}}
+					>
+						다시하기
+					</button>
+				</div>
+			)}
+
 			<div style={boardStyle}>
 				{gameBoard.map((tile, index) => {
 					return (
 						<Tile
-							index={index}
-							setTurn={setTurn}
-							setGameBoard={setGameBoard}
-							turn={turn}
+							key={index}
 							type={tile}
+							index={index}
+							handleTileClick={handleTileClick}
 						/>
 					);
 				})}
